bfe14419cb2b248aa8c2ac2b06c4a81f8117c787
Christopher Montiel <christopher.montiel.m@gmail.com>
2024-09-21 02:27:31 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI, Request
+from twilio.twiml.messaging_response import MessagingResponse
+import requests
+from dotenv import load_dotenv
+import os
+from groq import Groq
+
+# Cargar variables de entorno desde el archivo .env
+load_dotenv()
+
+# Inicializa FastAPI
+app = FastAPI()
+
+# URL base para el LLM local
+client = Groq(
+    api_key="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"
+)
+
+# Ruta para recibir los mensajes de WhatsApp
+@app.post("/whatsapp")
+async def whatsapp_webhook(request: Request):
+    # Obtén el contenido del mensaje entrante
+    form_data = await request.form()
+    incoming_msg = form_data.get('Body')
+# Procesa el mensaje usando OpenAI (GPT)
+    try:
+        chat_completion = client.chat.completions.create(
+            model="llama3-8b-8192",
+            messages=[
+                {"role": "system", "content": "Eres un asistente amigable, te debes expresar con emojis y siempre debes dar respuestas sencillas, cortas y en español."},
+                {"role": "user", "content": incoming_msg}
+            ]
+        )
+        reply = chat_completion.choices[0].message.content
+    except Exception as e:
+        reply = f"Lo siento, hubo un error: {str(e)}"
+
+    # Responder usando Twilio
+    # resp = MessagingResponse()
+    # resp.message(reply)
+    return str(reply)
